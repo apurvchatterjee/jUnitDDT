ed60a4177eb4a1391fd26e4c974f0bb604653aa8
Apurv Chatterjee <apurvchatterjee@users.noreply.github.com>
2018-12-08 14:07:36 +0530

    Modified Base Class, added Props

--- src/main/java/com/base/TestBase.java
+++ src/main/java/com/base/TestBase.java
@@ -1,5 +1,38 @@
 package com.base;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.events.EventFiringWebDriver;
+
 public class TestBase {
 
+	public static WebDriver dr = null;
+	public static EventFiringWebDriver driver = null;
+	private static FileInputStream fin = null;
+
+	public static final String PROJPATH = System.getProperty("user.dir");
+
+	public static Properties CONFIG = null;
+	public static Properties OR = null;
+	public static Properties logger = null;
+
+	public static void doInitialize() {
+		// initialize the property files
+		CONFIG = new Properties();
+		try {
+			fin = new FileInputStream(PROJPATH + "/src/main/resources/properties/config.properties");
+			CONFIG.load(fin);
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		if (driver == null) {
+
+		}
+	}
 }
