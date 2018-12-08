3db790df0bea290f0d2b2d6ba62fcc12a0e0202b
Apurv Chatterjee <apurvchatterjee@users.noreply.github.com>
2018-12-08 15:24:42 +0530

    Modified TestBase class

--- src/main/java/com/base/TestBase.java
+++ src/main/java/com/base/TestBase.java
@@ -6,33 +6,64 @@ import java.io.IOException;
 import java.util.Properties;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.support.events.EventFiringWebDriver;
 
 public class TestBase {
 
 	public static WebDriver dr = null;
 	public static EventFiringWebDriver driver = null;
-	private static FileInputStream fin = null;
-
 	public static final String PROJPATH = System.getProperty("user.dir");
 
 	public static Properties CONFIG = null;
 	public static Properties OR = null;
-	public static Properties logger = null;
+	public static Properties LOGGER = null;
+
+	private static FileInputStream fin;
 
 	public static void doInitialize() {
-		// initialize the property files
-		CONFIG = new Properties();
-		try {
-			fin = new FileInputStream(PROJPATH + "/src/main/resources/properties/config.properties");
-			CONFIG.load(fin);
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		if (driver == null) {
 
+		if (driver == null) {
+			// initialize CONFIG property files
+			CONFIG = new Properties();
+			OR = new Properties();
+			LOGGER = new Properties();
+
+			try {
+				fin = new FileInputStream(PROJPATH + "src/main/resources/config.properties");
+				CONFIG.load(fin);
+
+				fin = new FileInputStream(PROJPATH + "src/main/resources/or.properties");
+				OR.load(fin);
+
+				fin = new FileInputStream(PROJPATH + "src/main/resources/log4j.properties");
+				LOGGER.load(fin);
+
+			} catch (FileNotFoundException e) {
+				e.printStackTrace();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+
+			// initialize web driver now
+			if (CONFIG.getProperty("browser").equalsIgnoreCase("IE")) {
+				System.setProperty(CONFIG.getProperty("ie_key"), CONFIG.getProperty("ie_driverpath"));
+				dr = new InternetExplorerDriver();
+			} else if (CONFIG.getProperty("browser").equalsIgnoreCase("FIREFOX")) {
+				System.setProperty(CONFIG.getProperty("ff_key"), CONFIG.getProperty("ff_driverpath"));
+				dr = new FirefoxDriver();
+			} else if (CONFIG.getProperty("browser").equalsIgnoreCase("CHROME")) {
+				System.setProperty(CONFIG.getProperty("gc_key"), CONFIG.getProperty("gc_driverpath"));
+				dr = new ChromeDriver();
+			}
+			driver = new EventFiringWebDriver(dr);
 		}
+	}
+
+	public static void main(String[] args) {
+		doInitialize();
+		System.out.println(CONFIG.getProperty("testsite"));
 	}
 }
---
+++ src/main/java/com/commonfunctions/CommonFunctions.java
@@ -0,0 +1,16 @@
+package com.commonfunctions;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
+
+import com.base.TestBase;
+
+public class CommonFunctions extends TestBase {
+
+	
+
+	
+
+}
