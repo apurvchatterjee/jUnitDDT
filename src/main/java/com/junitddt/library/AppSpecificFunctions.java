f21b570331fce70d10c2cf64ebfdb7a56287943f
Apurv Chatterjee <apurvchatterjee@users.noreply.github.com>
2019-02-09 11:57:50 +0530

    Added log4j, modified OR and AppSpecific Func Lib

--- src/main/java/com/junitddt/library/AppSpecificFunctions.java
+++ src/main/java/com/junitddt/library/AppSpecificFunctions.java
@@ -7,19 +7,29 @@ public class AppSpecificFunctions extends TestBase {
 	public static void doLogin(String username, String password) {
 
 		if (isLoggedIn) {
-			return;
+			doLogout();
 		}
-		
+
 		try {
 			getObject("link_signin_xpath").click();
 			getObject("input_username_xpath").sendKeys(username);
 			getObject("input_password_xpath").sendKeys(password);
 			getObject("button_submit_xpath").click();
-			
+
+			if (getObject("link_signoff_xpath").isDisplayed()) {
+				isLoggedIn = true;
+			}
+
 			isLoggedIn = true;
 		} catch (Throwable e) {
 			System.out.println("error signing in.. ");
 		}
 	}
 
+	public static void doLogout() {
+		if (getObject("link_signoff_xpath").isDisplayed()) {
+			getObject("link_signoff_xpath").click();
+			isLoggedIn = false;
+		}
+	}
 }
